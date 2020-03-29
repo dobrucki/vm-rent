c9c50ad03b5029a1a0966f60acd76531f4c405a8
Jedrzej Dobrucki <dobrucki2@gmail.com>
2020-03-29 18:35:15 +0200

    Fix CreateVirtualMachineHandler

--- src/Application.Core/Services/VirtualMachineUseCases/CreateVirtualMachineHandler.cs
+++ src/Application.Core/Services/VirtualMachineUseCases/CreateVirtualMachineHandler.cs
@@ -1,62 +1,62 @@
-//using System;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using Application.Core.Dtos;
-//using Application.Core.Dtos.Requests;
-//using Application.Core.Models;
-//using Application.Core.Ports;
-//using MediatR;
-//using Microsoft.Extensions.Logging;
-//
-//
-//namespace Application.Core.Services.VirtualMachineUseCases
-//{
-//    public class CreateVirtualMachineHandler : 
-//        IRequestHandler<CreateVirtualMachineRequest, BaseResponseDto<VirtualMachineDto>>
-//    {
-//        private readonly IRepository<VirtualMachine> _repository;
-//        private readonly ILogger<CreateVirtualMachineHandler> _logger;
-//        private readonly IMediator _mediator;
-//
-//        public CreateVirtualMachineHandler(
-//            IRepository<VirtualMachine> repository,
-//            IMediator mediator, 
-//            ILogger<CreateVirtualMachineHandler> logger)
-//        {
-//            _repository = repository;
-//            _mediator = mediator;
-//            _logger = logger;
-//        }
-//        
-//        public async Task<BaseResponseDto<VirtualMachineDto>> Handle(
-//            CreateVirtualMachineRequest request, 
-//            CancellationToken cancellationToken)
-//        {
-//            var response = new BaseResponseDto<VirtualMachineDto>();
-//
-//            try
-//            {
-//                var virtualMachine = new VirtualMachine
-//                {
-//                    Id = Guid.NewGuid(),
-//                    Name = request.Name
-//                };
-//
-//                await _repository.AddAsync(virtualMachine);
-//                response.Data = new VirtualMachineDto
-//                {
-//                    Id = virtualMachine.Id,
-//                    Name = virtualMachine.Name
-//                };
-//                _logger.LogInformation($"Created virtual machine with id {virtualMachine.Id}. ");
-//            }
-//            catch (Exception ex)
-//            {
-//                _logger.LogError(ex, ex.Message);
-//                response.Errors.Add("An error occurred while creating the virtual machine.");
-//            }
-//
-//            return response;
-//        }
-//    }
-//}
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Core.Dtos;
+using Application.Core.Dtos.Requests;
+using Application.Core.Models;
+using Application.Core.Ports;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+
+namespace Application.Core.Services.VirtualMachineUseCases
+{
+    public class CreateVirtualMachineHandler : 
+        IRequestHandler<CreateVirtualMachineRequest, BaseResponseDto<VirtualMachineDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CreateVirtualMachineHandler> _logger;
+
+        public CreateVirtualMachineHandler(
+            ILogger<CreateVirtualMachineHandler> logger, 
+            IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+        
+        public async Task<BaseResponseDto<VirtualMachineDto>> Handle(
+            CreateVirtualMachineRequest request, 
+            CancellationToken cancellationToken)
+        {
+            var response = new BaseResponseDto<VirtualMachineDto>();
+
+            try
+            {
+                await using (_unitOfWork)
+                {
+                    var virtualMachine = new VirtualMachine
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = request.Name
+                    };
+                    await _unitOfWork.VirtualMachines.AddAsync(virtualMachine);
+                    response.Data = new VirtualMachineDto
+                    {
+                        Id = virtualMachine.Id,
+                        Name = virtualMachine.Name
+                    };
+                    await _unitOfWork.Complete();
+                    _logger.LogInformation($"Created virtual machine with id {virtualMachine.Id}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                response.Errors.Add("An error occurred while creating the virtual machine.");
+            }
+
+            return response;
+        }
+    }
+}

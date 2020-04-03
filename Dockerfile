c1cc5a89cc42b498c905022e55a9f497256cb631
Jedrzej Dobrucki <dobrucki2@gmail.com>
2020-04-03 14:29:20 +0200

    Add docker support

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,5 @@ RUN dotnet publish -c Release -o out
 
 FROM mcr.microsoft.com/dotnet/core/aspnet:3.1 as runtime
 WORKDIR /app
-COPY --from=build /app/src/UserInterface.RestApi/out .
+COPY --from=build /app/out .
 ENTRYPOINT ["dotnet", "UserInterface.RestApi.dll"]
--- src/UserInterface.RestApi/Startup.cs
+++ src/UserInterface.RestApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace UserInterface.RestApi
 {
@@ -29,18 +30,19 @@ namespace UserInterface.RestApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, PostgresContext context)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+            // else
+            // {
+            //     app.UseHsts();
+            // }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+            context.Database.Migrate();
 
             app.UseRouting();
 
